49403ab06f657362de6a392816d492ba60601012
tokimay <tokimay@gmail.com>
2025-02-19 13:24:31 +0330

    1403.12

--- protocols/client.py
+++ protocols/client.py
@@ -18,48 +18,21 @@ class Worker:
         self._subscribed = False
         self._authorized = False
         self._hasPriorJob = False
-        self._extranonceSubscribe = False
-        self._lastJobId = ''
+        self._Job_Ids = []
         self._extraNonce1 = ''
-        self._cionBase1 = ''
-        self._coinBase2 = ''
         self._currentDifficulty = globalVariable.START_DIFFICULTY
         self._lastShareTime = datetime.now()
         self._blockFound = 0
         self._foundedShare = []
 
-    def get_reader(self) -> asyncio.streams.StreamReader:
-        return self._reader
-
     def get_writer(self) -> asyncio.streams.StreamWriter:
         return self._writer
-
-    def get_worker_id(self) -> int:
-        return self._id
-
-    def set_worker_id(self, worker_id: int):
-        self._id = worker_id
-
-    def get_worker_name(self) -> str:
-        return self._name
 
     def set_worker_name(self, worker_name: str):
         self._name = worker_name
 
-    def get_worker_pass(self) -> str:
-        return self._password
-
     def set_worker_pass(self, worker_pass: str):
         self._password = worker_pass
-
-    def get_worker_ip(self) -> str:
-        return self._ip
-
-    def set_worker_ip(self, worker_ip: str):
-        self._ip = worker_ip
-
-    def get_worker_soft(self) -> str:
-        return self._softWare
 
     def set_worker_soft(self, worker_soft: str):
         self._softWare = worker_soft
@@ -88,17 +61,11 @@ class Worker:
     def set_has_prior_job(self, status: bool):
         self._hasPriorJob = status
 
-    def get_extranonce_subscribe(self, status: bool):
-        self._extranonceSubscribe = status
+    def get_last_job_id(self) -> list:
+        return self._Job_Ids
 
-    def set_extranonce_subscribe(self, status: bool):
-        self._extranonceSubscribe = status
-
-    def get_last_job_id(self):
-        return self._lastJobId
-
-    def set_last_job_id(self, status: str):
-        self._lastJobId = status
+    def set_last_job_id(self, _job_id: str):
+        self._Job_Ids.append(_job_id)
 
     def get_worker_extra_nonce1(self) -> str:
         return self._extraNonce1
@@ -118,29 +85,11 @@ class Worker:
     def set_last_share_time(self, time: datetime):
         self._lastShareTime = time
 
-    def get_block_found(self) -> int:
-        return self._blockFound
-
     def set_block_found(self):
         self._blockFound += 1
 
-    def get__founded_share(self) -> list:
-        return self._foundedShare
-
     def set_founded_share(self, difficulty: float):
         self._foundedShare.append(difficulty)
-
-    def get_coinbase1(self):
-        return self._cionBase1
-
-    def set_coinbase1(self, cion_base1: str):
-        self._cionBase1 = cion_base1
-
-    def get_coin_base2(self):
-        return self._coinBase2
-
-    def set_coin_base2(self, coin_base2: str):
-        self._coinBase2 = coin_base2
 
     async def get_client_request(self) -> dict:
         try:
